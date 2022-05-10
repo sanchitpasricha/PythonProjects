ed64e6672873d297ce8dd2fa643c77dd5a0ccc97
Sanchit Pasricha <sanchitpasricha@Sanchits-MacBook-Air.local>
2022-05-10 18:54:17 +0530

    Trivia

---
+++ Trivia/data.py
@@ -0,0 +1,37 @@
+question_data = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first computer bug was formed by faulty wires.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "FLAC stands for Free Lossless Audio Condenser&quot;&#039;",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The very first recorded computer was a moth found inside a Harvard Mark II computer.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "Windows is a monolithic kernel.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book "
+                 "form. ",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "MacOS is based on Linux.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The first dual-core CPU was the Intel Pentium D.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "Android versions are named in alphabetical order.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "A Boolean value of represents which of these words?",
+     "correct_answer": "False", "incorrect_answers": ["True"]}
+]
